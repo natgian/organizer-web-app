53747c18aa2fa4ef630172f554380e7eed8b8163
Nathalie Giancaspro <n.giancaspro@outlook.com>
2023-06-28 21:18:28 +0200

    replaced html files by ejs files, created pages and partials folder in views, created two partials header and footer

--- app.js
+++ app.js
@@ -7,9 +7,23 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.static("./public"));
 
+
+// ROUTES
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.get("/home", (req, res) => {
+  res.render("pages/home");
+})
+
+app.get("/registration", (req, res) => {
+  res.render("pages/registration");
+})
+
+app.get("/login", (req, res) => {
+  res.render("pages/login");
+})
 
 app.listen(3000, () => {
   console.log("SERVING ON PORT 3000");
