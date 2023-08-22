f2423a1e9cc4e6297ecfc4f14738ce2414e935b8
Nathalie Giancaspro <n.giancaspro@outlook.com>
2023-08-22 20:51:24 +0200

    Added autofocus on inputs, adjusted displayItemInput.js file. Changed navigation header, added bottom-border.

--- public/JS/displayItemInput.js
+++ public/JS/displayItemInput.js
@@ -1,6 +1,8 @@
 const addItemBtn = document.getElementById("add-item-btn");
 const newItemContainer = document.querySelector(".new-item-container");
+const inputElement = document.querySelector(".input");
 
 addItemBtn.addEventListener("click", () => {
   newItemContainer.style.display = "block";
+  inputElement.focus();
 });
