729ee4a033c9e329afab4c1e1683b92f34a2ae12
Nathalie Giancaspro <n.giancaspro@outlook.com>
2023-08-28 21:09:50 +0200

    Started styling the notes index page.

--- controllers/notizenController.js
+++ controllers/notizenController.js
@@ -1,11 +1,11 @@
 const Note = require("../models/note");
 
-// Render Listen Index Page
+// Render Notizen Index Page
 module.exports.index = async (req, res) => {
   res.render("pages/notizen");
 };
 
-// Render New NOte Page
+// Render New Note Page
 module.exports.renderNewNote = (req, res) => {
   res.render("notes/new");
 };
