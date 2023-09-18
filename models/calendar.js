9faf9e77e7a1de0b7031a4aa0d66fac0eb7fb2f9
Nathalie Giancaspro <n.giancaspro@outlook.com>
2023-09-18 21:01:13 +0200

    Corrected typo in calendar model.

--- models/calendar.js
+++ models/calendar.js
@@ -12,7 +12,7 @@ const calendarSchema = new mongoose.Schema({
   description: {
     type: String,
     required: true
-  }
+  },
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "User"
